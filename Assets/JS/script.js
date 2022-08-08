1c7f2bb047724f87f45da0d6210ff290855cad9f
Clay McKee <cmckee120993@gmail.com>
2022-08-07 20:42:47 -0700

    I have added functionality to the button and input using JQuery so that the input is saved to localStorage and then saved in a list item created by JS after hitting submit. For user ease, I have also cleared the search bar after clicking submit.

---
+++ Assets/JS/script.js
@@ -0,0 +1,13 @@
+// Submit button and user input 
+let submit = $('.btn');
+
+
+submit.on('click', function(event) {
+    let userInput = $(this).siblings('#city').val();
+    localStorage.setItem('currentCity', userInput);
+    $(this).siblings('#city').val('');
+    let cityHistory = $('.search-history');
+    let currentCity = localStorage.getItem('currentCity');
+    $('.search-history').append($(`<li>${currentCity}</li>`));
+
+})
