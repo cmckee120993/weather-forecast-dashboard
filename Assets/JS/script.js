131f4eb0e3ed7092e464cac83b63f0d8f487f491
Clay McKee <cmckee120993@gmail.com>
2022-08-08 08:26:24 -0700

    I tested out my geolocation API by using a test city. I was able to fetch the data and then return it to the console to find the longitude and lattitude.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -10,4 +10,19 @@ submit.on('click', function(event) {
     let currentCity = localStorage.getItem('currentCity');
     $('.search-history').append($(`<li>${currentCity}</li>`));
 
-})
+    // Taking localStorage to the weather api for current conditions
+
+    let currentConditions = function(event) {
+    let currentAPI = 'YTg0OTY5YTdlOTI2NDE5ODk4MzE0ZmViMThmNmIyMmU6YjQzYjhkMWYtODVhMi00M2Y0LTk4NzktNmU1ZThjMzdhYjhj';
+    let currentURL = 'https://api.myptv.com/geocoding/v1/locations/by-text?searchText=aan%27t%20verlaat%2033f%20&apiKey=YTg0OTY5YTdlOTI2NDE5ODk4MzE0ZmViMThmNmIyMmU6YjQzYjhkMWYtODVhMi00M2Y0LTk4NzktNmU1ZThjMzdhYjhj';
+    fetch(currentURL)
+    .then(function(response) {
+        console.log(response);
+        return response.json();})
+        .then(function(data) {
+            console.log(data);
+        });
+}
+currentConditions()
+});
+
